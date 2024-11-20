05752a5def2a777cbb1840106f1f0d98ccecde43
Yifei Zhou <4608342+evianzhow@users.noreply.github.com>
2024-11-20 12:08:13 -0500

    Updated MQTT broker information

--- smart_farm_backend/app/config.py
+++ smart_farm_backend/app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    MQTT_BROKER = os.getenv("MQTT_BROKER", "broker.hivemq.com")
-    MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
+    MQTT_BROKER = os.getenv("MQTT_BROKER", "06a68c084516440da5d6c84b6514ed49.s1.eu.hivemq.cloud")
+    MQTT_PORT = int(os.getenv("MQTT_PORT", 8883))
     DB_URI = os.getenv("DB_URI", "sqlite:///iot_data.db")
